170c618e6798bd0b387880914b9e471576c4fd7c
Kef1r1k <enikiforoff2@yandex.ru>
2025-01-24 23:11:47 +0500

    hint js

---
+++ src/javascript/hint.js
@@ -0,0 +1,43 @@
+const hints = document.getElementsByClassName('M_ArticleContents')
+
+function hintInit() {
+  for (let i = 0; i < hints.length; i++) {
+    const hint = hints[i]
+    hintOpen(hint)
+  }
+}
+
+function hintOpen(hint) {
+  const hintButton = hint.querySelector('.A_ToggleButton')
+  const hintText = document.querySelector('.A_TextContents.hint')
+  const articleTexts = document.querySelectorAll('.A_TextContents.full')
+  const articleQuotes = document.querySelectorAll('.M_ArticleQuote')
+
+  hintButton.addEventListener('click', (e) => {
+    hintButton.classList.toggle('active')
+
+    if (hintButton.classList.contains('active')) {
+      hintText.classList.remove('hidden')
+      for (let i = 0; i < articleTexts.length; i++) {
+        const articleText = articleTexts[i]
+        articleText.classList.add('hidden')
+      }
+      for (let i = 0; i < articleQuotes.length; i++) {
+        const articleQuote = articleQuotes[i]
+        articleQuote.classList.add('hidden')
+      }
+    } else {
+      for (let i = 0; i < articleTexts.length; i++) {
+        const articleText = articleTexts[i]
+        articleText.classList.remove('hidden')
+      }
+      for (let i = 0; i < articleQuotes.length; i++) {
+        const articleQuote = articleQuotes[i]
+        articleQuote.classList.remove('hidden')
+      }
+      hintText.classList.add('hidden')
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', hintInit())
