b585465c826092869cf786afacaf1f75c2a3d345
Kef1r1k <enikiforoff2@yandex.ru>
2025-03-10 20:39:29 +0500

    fixes

--- src/javascript/video.js
+++ src/javascript/video.js
@@ -1,9 +1,16 @@
 const video = document.getElementById('bg-video')
+const mediaphone = window.matchMedia(
+  '(min-width: 320px) and (max-width: 730px)'
+)
 
-window.addEventListener('scroll', () => {
-  video.pause()
-})
+if (mediaphone.matches) {
+  video.classList.add('none')
+} else {
+  window.addEventListener('scroll', () => {
+    video.pause()
+  })
 
-window.addEventListener('scrollend', () => {
-  video.play()
-})
+  window.addEventListener('scrollend', () => {
+    video.play()
+  })
+}
