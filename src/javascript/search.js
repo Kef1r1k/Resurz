50682b75f2696e89f4aa9cecc474ed4e582c2422
Kef1r1k <enikiforoff2@yandex.ru>
2025-05-03 15:49:04 +0500

    global search done

--- src/javascript/search.js
+++ src/javascript/search.js
@@ -22,14 +22,16 @@ function displayResults(articles) {
   const resultsContainer = document.querySelector('.C_Articles')
   const noResults = document.querySelector('.A_PageCover')
 
+  noResults.style.display = 'none'
+  resultsContainer.innerHTML = ''
+
   if (articles.length === 0) {
     resultsContainer.style.display = 'none'
     noResults.style.display = 'block'
     return
   }
 
-  noResults.style.display = 'none'
-  resultsContainer.innerHTML = ''
+  resultsContainer.style.display = 'flex'
 
   articles.forEach((article) => {
     const teaser = createTeaser(article)
@@ -60,9 +62,28 @@ function createTeaser(article) {
 }
 
 async function performSearch() {
-  const articles = await getArticles()
-  const filteredArticles = filterArticles(articles, getSearchQuery())
-  displayResults(filteredArticles, getSearchQuery())
+  const loader = document.querySelector('.A_LoaderSpinner')
+  loader.style.display = 'block'
+
+  const noResults = document.querySelector('.A_PageCover')
+  noResults.style.display = 'none'
+  const resultsContainer = document.querySelector('.C_Articles')
+  resultsContainer.style.display = 'none'
+  resultsContainer.innerHTML = ''
+
+  try {
+    const articles = await getArticles()
+    const filteredArticles = filterArticles(articles, getSearchQuery())
+    displayResults(filteredArticles)
+  } catch (error) {
+    console.error('Search error:', error)
+    resultsContainer.innerHTML = `
+        <p>Произошла ошибка при загрузке</p>
+    `
+    resultsContainer.style.display = 'block'
+  } finally {
+    loader.style.display = 'none'
+  }
 }
 
 document.addEventListener('DOMContentLoaded', performSearch)
